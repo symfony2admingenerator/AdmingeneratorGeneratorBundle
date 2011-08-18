8f34f790250afa3aa48409105c732ae1a1d75540
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-19 01:54:52 +0200

    Fix useless import

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -2,9 +2,6 @@
 
 namespace Admingenerator\GeneratorBundle\Tests;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
-
 /*
  * @author Cedric LOMBARDOT
  */
