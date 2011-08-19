d9613c0157e4dca7a29c46a9369d518dfca6dbf2
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-19 02:40:21 +0200

    Add tests for ClassLoader #2

---
+++ Tests/ClassLoader/AdmingeneratedClassLoaderTest.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Admingenerator\GeneratorBundle\Tests\ClassLoader;
+
+use Admingenerator\GeneratorBundle\Tests\TestCase;
+
+use Admingenerator\GeneratorBundle\ClassLoader\AdmingeneratedClassLoader;
+
+class AdmingeneratedClassLoaderTest extends TestCase
+{
+    /**
+     * @dataProvider getLoadClassTests
+     */
+    public function testLoadClass($className, $testClassName, $message)
+    {
+        $loader = new AdmingeneratedClassLoader();
+        $loader->setBasePath(realpath(sys_get_temp_dir()));
+        $loader->loadClass($testClassName);
+        $this->assertTrue(class_exists($className), $message);
+    }
+
+    public function getLoadClassTests()
+    {
+        return array(
+            array('\\Admingenerated\\AdmingeneratorDemoBundle\\BaseController\\ListController', 'Admingenerated\\AdmingeneratorDemoBundle\\BaseController\\ListController',   '->loadClass() loads admingenerated class'),
+        );
+    }
+}
