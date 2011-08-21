dfad768cb9e65f08ce35179ca9ffa94786b7e059
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-21 12:26:26 +0200

    Bootsrap tests

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -2,8 +2,6 @@
 
 namespace Admingenerator\GeneratorBundle\Tests;
 
-require_once realpath(__DIR__.'/../../../../../app/AppKernel.php');
-
 /*
  * @author Cedric LOMBARDOT
  */
---
+++ Tests/bootstrap.php
@@ -0,0 +1,5 @@
+<?php 
+
+require __DIR__.'/../../../../../app/bootstrap.php.cache';
+
+require __DIR__.'/../../../../../app/AppKernel.php';
