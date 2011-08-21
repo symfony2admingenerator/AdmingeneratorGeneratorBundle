998eaca448a77b40cc4b784743cda79408db6f47
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-21 12:09:36 +0200

    Update QueryFilterInteface

--- QueryFilter/BaseQueryFilter.php
+++ QueryFilter/BaseQueryFilter.php
@@ -3,25 +3,44 @@
 namespace Admingenerator\GeneratorBundle\QueryFilter; 
 
 
-class BaseQueryFilter implements QueryFilterInterface
+abstract class BaseQueryFilter implements QueryFilterInterface
 {
     protected $query;
     
+    /**
+     * (non-PHPdoc)
+     * @see GeneratorBundle\QueryFilter.QueryFilterInterface::setQuery()
+     */
     public function setQuery($query)
     {
         $this->query = $query;
     }
     
+    /**
+     * (non-PHPdoc)
+     * @see GeneratorBundle\QueryFilter.QueryFilterInterface::getQuery()
+     */
     public function getQuery()
     {
         return $this->query;
     }
     
+    /**
+     * (non-PHPdoc)
+     * @see \GeneratorBundle\QueryFilter.QueryFilterInterface::addDefaultFilter()
+     */
     public function addDefaultFilter($field, $value)
     {
         throw new \LogicException('No method defined to execute this type of filters');
     }
     
+    /**
+     * 
+     * By default we call addDefaultFilter
+     * 
+     * @param $name
+     * @param $values
+     */
     public function __call($name, $values = array())
     {
         if (preg_match('/add(.+)Filter/', $name)) {
--- QueryFilter/QueryFilterInterface.php
+++ QueryFilter/QueryFilterInterface.php
@@ -4,11 +4,29 @@ namespace Admingenerator\GeneratorBundle\QueryFilter;
 
 interface QueryFilterInterface
 {
+    /**
+     * @param object $query the query object interface depend of the ORM
+     * 
+     * @api
+     */
     function setQuery($query);
     
+    /**
+     * @return the query object interface depend of the ORM
+     * 
+     * @api
+     */
     function getQuery();
     
+    /**
+     * Add filter for Default db types (types, not found
+     * by others add*Filter() methods
+     * 
+     * @param string $field the db field
+     * @param string $value the search value
+     * 
+     * @api
+     */
     function addDefaultFilter($field, $value);
     
-    function __call($name, $value = array());
 }
