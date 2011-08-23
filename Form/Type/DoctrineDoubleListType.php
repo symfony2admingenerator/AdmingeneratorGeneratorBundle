f7be9c22c3779f8b63d1932ea9e4661532b741c9
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-08-23 03:21:12 +0200

    Start an implementation of double_list

---
+++ Form/Type/DoctrineDoubleListType.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Admingenerator\GeneratorBundle\Form\Type;
+
+
+use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToChoicesTransformer;
+
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityChoiceList;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+
+use Symfony\Bridge\Doctrine\Form\EventListener\MergeCollectionListener;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\EntitiesToArrayTransformer;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\EntityToIdTransformer;
+
+class DoctrineDoubleListType extends AbstractType
+{
+
+    protected $registry;
+    
+    protected $choices;
+
+    public function __construct(RegistryInterface $registry)
+    {
+        $this->registry = $registry;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+         //$builder->appendClientTransformer(new ArrayToChoicesTransformer());
+        
+       //var_dump($options['choice_list']); 
+     //   die;
+            
+         $builder
+                ->addEventSubscriber(new MergeCollectionListener())
+                ->prependClientTransformer(new ArrayToChoicesTransformer($options['choice_list']))
+                ->prependClientTransformer(new EntitiesToArrayTransformer($options['choice_list']))
+                ;   
+            
+        $this->choices = $options['choice_list']->getChoices();
+        
+        $builder->add('selected', 'choice', array(
+            'multiple' => true,
+            'choices' => $this->choices,
+        ));
+        
+        $builder->add('unselected', 'choice', array(
+            'multiple' => true,
+            'choices' => $this->choices,
+        ));
+       
+    }
+    
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form)
+    {
+        $selecteds = array_flip($view->get('value'));
+        $choices_selected = $choices_unselected = array();
+        
+        //Rebuilds choices
+        foreach($this->choices as $key => $choice) {
+            if (isset($selecteds[$key])) {
+                $choices_selected[$key] = $choice;
+            } else {
+                $choices_unselected[$key] = $choice;
+            }
+        }
+        
+        $view->set('choices_selected', $choices_selected);
+        $view->set('choices_unselected', $choices_unselected);
+    }
+    
+    public function getParent(array $options)
+    {
+        return 'field';
+    }
+    
+    public function getDefaultOptions(array $options)
+    {
+        $defaultOptions = array(
+            'em'                => null,
+            'class'             => null,
+            'property'          => null,
+            'query_builder'     => null,
+            'choices'           => array(),
+        );
+
+        $options = array_replace($defaultOptions, $options);
+
+        if (!isset($options['choice_list'])) {
+            $defaultOptions['choice_list'] = new EntityChoiceList(
+                $this->registry->getEntityManager($options['em']),
+                $options['class'],
+                $options['property'],
+                $options['query_builder'],
+                $options['choices']
+            );
+        }
+
+        return $defaultOptions;
+    }
+    
+    public function getName()
+    {
+        return 'doctrine_double_list';
+    }
+}
