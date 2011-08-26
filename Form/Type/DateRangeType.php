df238c1e21a3891a810207f9589942f783d96e3f
clombardot <cedric.lombardot@gmail.com>
2011-08-26 10:45:21 +0200

    Add option for date_rage to format field

--- Form/Type/DateRangeType.php
+++ Form/Type/DateRangeType.php
@@ -12,30 +12,37 @@ use Symfony\Component\Form\FormInterface;
 
 class DateRangeType extends AbstractType
 {
-    
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilder $builder, array $options)
     {
          unset($options['years']);
-         
+
          $options['from']['required'] = $options['required'];
          $options['to']['required'] = $options['required'];
-         
+
+         if ($options['format']) {
+            $options['from']['format'] = $options['format'];
+            $options['to']['format'] = $options['format'];
+         }
+
+
          $builder
                ->add('from', new DateType(), $options['from'])
                ->add('to', new DateType(), $options['to']);
     }
-    
+
     public function getParent(array $options)
     {
         return 'form';
     }
-    
+
     public function getDefaultOptions(array $options)
     {
         $defaultOptions = array(
+            'format'            => null,
             'years'             => range(date('Y'), date('Y') - 120),
             'to'                => null,
             'from'              => null,
@@ -46,14 +53,14 @@ class DateRangeType extends AbstractType
         if (is_null($defaultOptions['to'])) {
             $defaultOptions['to'] = array('years' => $defaultOptions['years']);
         }
-        
+
         if (is_null($defaultOptions['from'])) {
             $defaultOptions['from'] = array('years' => $defaultOptions['years']);
-        } 
-        
+        }
+
         return $defaultOptions;
     }
-    
+
     public function getName()
     {
         return 'date_range';
