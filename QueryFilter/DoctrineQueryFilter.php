0d567b163f69425982c8ec8d3ae60b2574c863e8
clombardot <cedric.lombardot@gmail.com>
2011-08-26 16:54:15 +0200

    Fix error on date filter when you enter empty values

--- QueryFilter/DoctrineQueryFilter.php
+++ QueryFilter/DoctrineQueryFilter.php
@@ -4,13 +4,13 @@ namespace Admingenerator\GeneratorBundle\QueryFilter;
 
 class DoctrineQueryFilter extends BaseQueryFilter
 {
-    
+
     public function addDefaultFilter($field, $value)
     {
         $this->query->andWhere(sprintf('q.%s = :%s',$field, $field));
         $this->query->setParameter($field, $value);
     }
-    
+
     public function addBooleanFilter($field, $value)
     {
         if ("" !== $value) {
@@ -18,42 +18,47 @@ class DoctrineQueryFilter extends BaseQueryFilter
             $this->query->setParameter($field, $value);
         }
     }
-    
+
     public function addStringFilter($field, $value)
     {
         $this->query->andWhere(sprintf('q.%s LIKE :%s',$field, $field));
         $this->query->setParameter($field, '%'.$value.'%');
     }
-    
+
     public function addCollectionFilter($field, $value)
     {
         if (!is_array($value)) {
             $value = array($value->getId());
         }
-        
+
         if (strstr($field, '.')) {
             list($table, $field) = explode('.', $field);
         } else {
             $table = $field;
             $field = $id;
         }
-        
+
         $this->query->leftJoin('q.'.$table, $table);
         $this->query->groupBy('q.id');
         $this->query->andWhere(sprintf('%s.%s IN (:%s)',$table, $field, $table.'_'.$field));
         $this->query->setParameter($table.'_'.$field, $value);
-        
+
     }
-    
+
     public function addDateFilter($field, $value)
     {
         if (is_array($value)) {
-            $this->query->andWhere(sprintf('q.%s >= :%s_from',$field, $field ));
-            $this->query->setParameter($field.'_from' , $value['from']->format('Y-m-d'));
-        
-            $this->query->andWhere(sprintf('q.%s <= :%s_to',$field, $field ));
-            $this->query->setParameter($field.'_to' , $value['to']->format('Y-m-d'));
-        } else {
+            if ($value['from']) {
+                $this->query->andWhere(sprintf('q.%s >= :%s_from',$field, $field ));
+                $this->query->setParameter($field.'_from' , $value['from']->format('Y-m-d'));
+            }
+
+            if ($value['to']) {
+                $this->query->andWhere(sprintf('q.%s <= :%s_to',$field, $field ));
+                $this->query->setParameter($field.'_to' , $value['to']->format('Y-m-d'));
+            }
+
+        } elseif($value instanceof \DateTime) {
             $this->query->andWhere(sprintf('q.%s = :%s',$field, $field ));
             $this->query->setParameter($field, $value->format('Y-m-d'));
         }
