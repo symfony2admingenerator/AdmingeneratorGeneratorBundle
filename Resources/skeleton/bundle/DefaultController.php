37e35b8284d51d91d35e213d046ab7a463ae080c
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-09-01 20:58:10 +0200

    Fix the bad base controller name #7

--- Resources/skeleton/bundle/DefaultController.php
+++ Resources/skeleton/bundle/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace {{ namespace }}\Controller;
 
-use Admingenerated\{{ bundle }}\BaseController\NewController as Base{{ action }}Controller;
+use Admingenerated\{{ bundle }}\BaseController\{{ action }}Controller as Base{{ action }}Controller;
 
 class {{ action }}Controller extends Base{{ action }}Controller
 {
