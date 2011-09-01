573f2387628096eb94fa0f49f86732fd1b7c3be8
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-09-01 23:34:34 +0200

    Implement string filter

--- QueryFilter/DoctrineODMQueryFilter.php
+++ QueryFilter/DoctrineODMQueryFilter.php
@@ -9,5 +9,38 @@ class DoctrineODMQueryFilter extends BaseQueryFilter
     {
         $this->query->field($field)->equals($value);
     }
+    
+    public function addStringFilter($field, $value)
+    {
+        $this->query->field($field)->equals(new \MongoRegex("/.*$value.*/i"));
+    }
+    
+    public function addBooleanFilter($field, $value)
+    {
+        if ("" !== $value) {
+            $this->query->field($field)->equals($value);
+        }
+    }
+    
+    public function addDateFilter($field, $value)
+    {
+        if (is_array($value)) {
+            if ($value['from'] && !$value['to']) {
+                $this->query->field($field)->lte($value['from']->format('Y-m-d'));
+            }
 
+            if ($value['to'] && !$value['from']) {
+                $this->query->field($field)->gte($value['to']->format('Y-m-d'));
+            }
+
+            if ($value['to'] && $value['from']) {
+                $this->query->field($field)->range($value['from']->format('Y-m-d'), $value['to']->format('Y-m-d'));
+            }
+             
+        } elseif($value instanceof \DateTime) {
+            $this->query->field($field)->equals($value->format('Y-m-d'));
+        }
+    }
+
+    
 }
