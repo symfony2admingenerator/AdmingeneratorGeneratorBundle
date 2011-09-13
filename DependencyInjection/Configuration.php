13b1b8969dbbe776296db2068931474090f84b36
Cedric Lombardot <cedric.lombardot@gmail.com>
2011-09-13 21:49:25 +0200

    And of course i miss to cache clear => crash

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,7 +27,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode->children()
                     ->booleanNode('overwrite_if_exists')->defaultFalse()->end()
-                    ->scalarNode('knp_menu_class')->defaultValue("Admingenerator\GeneratorBundle\Menu\DefaultMenu")->end()
+                    ->scalarNode('knp_menu_class')->defaultValue("Admingenerator\GeneratorBundle\Menu\DefaultMenuBuilder")->end()
                 ->end();
 
         return $treeBuilder;
