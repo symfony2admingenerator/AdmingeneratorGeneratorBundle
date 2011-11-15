fa211b4742f233978e1ecaa24779395f0604cb4b
cedriclombardot <cedric.lombardot@gmail.com>
2011-11-16 00:26:45 +0100

    Because there is to long to load vendors for travis

--- vendor/install_vendors.php
+++ vendor/install_vendors.php
@@ -3,8 +3,12 @@
 
 set_time_limit(0);
 
+if (!isset($argv[1])) {
+    $argv[1] = 0;
+}
+
 $vendorDir = __DIR__;
-$deps = array(
+$deps_0 = array(
     array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
     array('pagerfanta', 'http://github.com/whiteoctober/Pagerfanta.git', 'origin/master'),
     array('WhiteOctober/PagerfantaBundle', 'http://github.com/whiteoctober/WhiteOctoberPagerfantaBundle.git', 'origin/master'),
@@ -13,6 +17,9 @@ $deps = array(
     array('symfony/Bundle/AsseticBundle', 'http://github.com/symfony/AsseticBundle.git', 'origin/master'),
     array('assetic', 'http://github.com/kriswallsmith/assetic.git', 'origin/master'),
     array('twig-generator', 'http://github.com/cedriclombardot/TwigGenerator.git', 'origin/master'),
+);
+
+$deps_1 = array(
     array('twig', 'http://github.com/fabpot/Twig.git', 'origin/master'),
     array('twig-extensions', 'http://github.com/fabpot/Twig-extensions.git', 'origin/master'),
     array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master'),
@@ -20,7 +27,9 @@ $deps = array(
     array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', 'origin/master'),
 );
 
-foreach ($deps as $dep) {
+$deps = 'deps_'.$argv[1];
+
+foreach ($$deps as $dep) {
     list($name, $url, $rev) = $dep;
 
     echo "> Installing/Updating $name\n";
