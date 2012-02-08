a192a29267bb03582918e754665c9fa7fe0867d9
cedriclombardot <cedric.lombardot@gmail.com>
2012-02-09 00:59:30 +0100

    Update propel double list

--- Form/Type/PropelDoubleListType.php
+++ Form/Type/PropelDoubleListType.php
@@ -2,95 +2,58 @@
 
 namespace Admingenerator\GeneratorBundle\Form\Type;
 
-
-use Symfony\Component\Validator\Constraints\ChoiceValidator;
-
-use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToChoicesTransformer;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
-use Symfony\Bridge\Propel1\Form\ChoiceList\ModelChoiceList;
-
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Bridge\Propel1\Form\Type\ModelType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 
-use Symfony\Bridge\Propel1\Form\EventListener\MergeCollectionListener;
-use Symfony\Bridge\Propel1\Form\DataTransformer\ModelsToArrayTransformer;
-use Symfony\Bridge\Propel1\Form\DataTransformer\ModelToIdTransformer;
-
-class PropelDoubleListType extends AbstractType
+class PropelDoubleListType extends ModelType
 {
-
-    protected $choices;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilder $builder, array $options)
-    {
-         $builder
-               ->prependClientTransformer(new ModelsToArrayTransformer($options['choice_list']))
-               ;
-
-        $this->choices = $options['choice_list']->getChoices();
-
-        unset($options['choices']);
-
-    }
-
-
     /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form)
     {
-        $values = $view->get('value');
+        $choiceList = $form->getAttribute('choice_list');
 
-        $selecteds = array_flip($values);
+        $choices = $choiceList->getChoices();
+        $indices =  $choiceList->getValuesForChoices($choices);
+        $selectedChoices = $choiceList->getChoicesForValues($view->get('value'));
+        $selectedIndices =  $choiceList->getValuesForChoices($selectedChoices);
+
         $choices_selected = $choices_unselected = array();
 
-        //Rebuilds choices
-        foreach ($this->choices as $key => $choice) {
-            if (isset($selecteds[$key])) {
-                $choices_selected[$key] = $choice;
+        foreach ($indices as $k => $indice) {
+            if (in_array($indice, $selectedIndices)) {
+                $choices_selected[] = array(
+                    'value' => $indice,
+                    'label' => $choices[$indice]
+                );
             } else {
-                $choices_unselected[$key] = $choice;
+                $choices_unselected[] = array(
+                    'value' => $indice,
+                    'label' => $choices[$indice]
+                );
             }
         }
 
-        $view->set('choices_selected', $choices_selected);
+        $view->set('choices_selected',  $choices_selected);
         $view->set('choices_unselected', $choices_unselected);
     }
 
-    public function getParent(array $options)
-    {
-        return 'field';
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultOptions(array $options)
     {
-        $defaultOptions = array(
-            'class'             => null,
-            'property'          => null,
-            'choices'           => array(),
-            'query_object'      => null,
-        );
+        $options = parent::getDefaultOptions($options);
+        $options['multiple'] = true;
 
-        $options = array_replace($defaultOptions, $options);
-
-        if (!isset($options['choice_list'])) {
-            $defaultOptions['choice_list'] = new ModelChoiceList(
-                $options['class'],
-                $options['property'],
-                $options['choices'],
-                $options['query_object']
-            );
-        }
-
-        return $defaultOptions;
+        return $options;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'propel_double_list';
