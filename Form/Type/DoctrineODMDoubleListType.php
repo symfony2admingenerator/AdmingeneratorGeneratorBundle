b6a297b201ca3a9046b3a2633aaeff4311fa6ef7
cedriclombardot <cedric.lombardot@gmail.com>
2012-02-09 01:04:19 +0100

    Update doctrine odm double list

--- Form/Type/DoctrineODMDoubleListType.php
+++ Form/Type/DoctrineODMDoubleListType.php
@@ -2,107 +2,58 @@
 
 namespace Admingenerator\GeneratorBundle\Form\Type;
 
-
-use Doctrine\ODM\MongoDB\DocumentManager;
-
-use Symfony\Component\Validator\Constraints\ChoiceValidator;
-
-use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToChoicesTransformer;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
-use Symfony\Bundle\DoctrineMongoDBBundle\Form\ChoiceList\DocumentChoiceList;
-
-use Symfony\Bridge\Doctrine\RegistryInterface;
-
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Doctrine\Bundle\MongoDBBundle\Form\Type\DocumentType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 
-use Symfony\Bridge\Doctrine\Form\EventListener\MergeCollectionListener;
-use Symfony\Bundle\DoctrineMongoDBBundle\Form\DataTransformer\DocumentsToArrayTransformer;
-
-class DoctrineODMDoubleListType extends AbstractType
+class DoctrineODMDoubleListType extends DocumentType
 {
-
-    protected $documentManager;
-
-    protected $choices;
-
-    public function __construct(DocumentManager $documentManager)
-    {
-        $this->documentManager = $documentManager;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilder $builder, array $options)
-    {
-         $builder
-               ->prependClientTransformer(new DocumentsToArrayTransformer($options['choice_list']))
-               ;
-
-        $this->choices = $options['choice_list']->getChoices();
-
-        unset($options['choices']);
-
-    }
-
-
     /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form)
     {
-        $values = $view->get('value');
+        $choiceList = $form->getAttribute('choice_list');
 
-        $selecteds = array_flip($values);
+        $choices = $choiceList->getChoices();
+        $indices =  $choiceList->getValuesForChoices($choices);
+        $selectedChoices = $choiceList->getChoicesForValues($view->get('value'));
+        $selectedIndices =  $choiceList->getValuesForChoices($selectedChoices);
+
         $choices_selected = $choices_unselected = array();
 
-        //Rebuilds choices
-        foreach ($this->choices as $key => $choice) {
-            if (isset($selecteds[$key])) {
-                $choices_selected[$key] = $choice;
+        foreach ($indices as $k => $indice) {
+            if (in_array($indice, $selectedIndices)) {
+                $choices_selected[] = array(
+                    'value' => $indice,
+                    'label' => $choices[$indice]
+                );
             } else {
-                $choices_unselected[$key] = $choice;
+                $choices_unselected[] = array(
+                    'value' => $indice,
+                    'label' => $choices[$indice]
+                );
             }
         }
 
-        $view->set('choices_selected', $choices_selected);
+        $view->set('choices_selected',  $choices_selected);
         $view->set('choices_unselected', $choices_unselected);
     }
 
-    public function getParent(array $options)
-    {
-        return 'field';
-    }
-
+    /**
+     * {@inheritdoc}
+     */
     public function getDefaultOptions(array $options)
     {
-        $defaultOptions = array(
-            'document_manager'  => $this->documentManager,
-            'class'             => null,
-            'property'          => null,
-            'query_builder'     => null,
-            'choices'           => array(),
-        );
+        $options = parent::getDefaultOptions($options);
+        $options['multiple'] = true;
 
-        $options = array_replace($defaultOptions, $options);
-
-        if (!isset($options['choice_list'])) {
-            $defaultOptions['choice_list'] = new DocumentChoiceList(
-                $options['document_manager'],
-                $options['class'],
-                $options['property'],
-                $options['query_builder'],
-                $options['choices']
-            );
-        }
-
-        return $defaultOptions;
+        return $options;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'doctrine_odm_double_list';
