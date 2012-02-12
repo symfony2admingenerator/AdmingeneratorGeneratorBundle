7b3c09a7980a83fb79139b3a6e6e383681b14426
cedriclombardot <cedric.lombardot@gmail.com>
2012-02-12 17:35:37 +0100

    Update travis install_vendors

--- vendor/install_vendors.php
+++ vendor/install_vendors.php
@@ -9,22 +9,22 @@ if (!isset($argv[1])) {
 
 $vendorDir = __DIR__;
 $deps_0 = array(
-    array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('pagerfanta', 'http://github.com/whiteoctober/Pagerfanta.git', 'origin/master'),
-    array('WhiteOctober/PagerfantaBundle', 'http://github.com/whiteoctober/WhiteOctoberPagerfantaBundle.git', 'origin/master'),
-    array('KnpMenu', 'http://github.com/knplabs/KnpMenu.git', 'origin/master'),
-    array('Knp/Bundle/MenuBundle', 'http://github.com/knplabs/KnpMenuBundle.git', 'origin/master'),
-    array('symfony/Bundle/AsseticBundle', 'http://github.com/symfony/AsseticBundle.git', 'origin/master'),
-    array('assetic', 'http://github.com/kriswallsmith/assetic.git', 'origin/master'),
-    array('twig-generator', 'http://github.com/cedriclombardot/TwigGenerator.git', 'origin/master'),
+    array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('pagerfanta', 'git://github.com/whiteoctober/Pagerfanta.git', 'origin/master'),
+    array('WhiteOctober/PagerfantaBundle', 'git://github.com/whiteoctober/WhiteOctoberPagerfantaBundle.git', 'origin/master'),
+    array('KnpMenu', 'git://github.com/KnpLabs/KnpMenu.git', 'origin/master'),
+    array('Knp/Bundle/MenuBundle', 'git://github.com/KnpLabs/KnpMenuBundle.git', 'origin/master'),
+    array('symfony/Bundle/AsseticBundle', 'git://github.com/symfony/AsseticBundle.git', 'origin/master'),
+    array('assetic', 'git://github.com/kriswallsmith/assetic.git', 'origin/master'),
+    array('twig-generator', 'git://github.com/cedriclombardot/TwigGenerator.git', 'origin/master'),
 );
 
 $deps_1 = array(
-    array('twig', 'http://github.com/fabpot/Twig.git', 'origin/master'),
-    array('twig-extensions', 'http://github.com/fabpot/Twig-extensions.git', 'origin/master'),
-    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master'),
-    array('doctrine', 'http://github.com/doctrine/doctrine2.git', 'origin/master'),
-    array('doctrine-dbal', 'http://github.com/doctrine/dbal.git', 'origin/master'),
+    array('twig', 'git://github.com/fabpot/Twig.git', 'origin/master'),
+    array('twig-extensions', 'git://github.com/fabpot/Twig-extensions.git', 'origin/master'),
+    array('doctrine-common', 'git://github.com/doctrine/common.git', 'origin/master'),
+    array('doctrine', 'git://github.com/doctrine/doctrine2.git', 'origin/master'),
+    array('doctrine-dbal', 'git://github.com/doctrine/dbal.git', 'origin/master'),
 );
 
 $deps = 'deps_'.$argv[1];
@@ -36,7 +36,7 @@ foreach ($$deps as $dep) {
 
     $installDir = $vendorDir.'/'.$name;
     if (!is_dir($installDir)) {
-        system(sprintf('git clone -q %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
     }
 
     system(sprintf('cd %s && git fetch -q origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
