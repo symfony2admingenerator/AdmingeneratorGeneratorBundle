9bfe5a6dfe1d2441313f02eb4e38e798c4d8e06c
clombardot <cedric.lombardot@gmail.com>
2012-03-08 12:11:54 +0100

    Revert propelDouble list for 2.0 compat #95

--- Form/Type/PropelDoubleListType.php
+++ Form/Type/PropelDoubleListType.php
@@ -2,58 +2,95 @@
 
 namespace Admingenerator\GeneratorBundle\Form\Type;
 
-use Symfony\Bridge\Propel1\Form\Type\ModelType;
+
+use Symfony\Component\Validator\Constraints\ChoiceValidator;
+
+use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToChoicesTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+use Propel\PropelBundle\Form\ChoiceList\ModelChoiceList;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 
-class PropelDoubleListType extends ModelType
+use Propel\PropelBundle\Form\EventListener\MergeCollectionListener;
+use Propel\PropelBundle\Form\DataTransformer\ModelsToArrayTransformer;
+use Propel\PropelBundle\Form\DataTransformer\ModelToIdTransformer;
+
+class PropelDoubleListType extends AbstractType
 {
+
+    protected $choices;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+         $builder
+               ->prependClientTransformer(new ModelsToArrayTransformer($options['choice_list']))
+               ;
+
+        $this->choices = $options['choice_list']->getChoices();
+
+        unset($options['choices']);
+
+    }
+
+
     /**
      * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form)
     {
-        $choiceList = $form->getAttribute('choice_list');
+        $values = $view->get('value');
 
-        $choices = $choiceList->getChoices();
-        $indices =  $choiceList->getValuesForChoices($choices);
-        $selectedChoices = $choiceList->getChoicesForValues($view->get('value'));
-        $selectedIndices =  $choiceList->getValuesForChoices($selectedChoices);
-
+        $selecteds = array_flip($values);
         $choices_selected = $choices_unselected = array();
 
-        foreach ($indices as $k => $indice) {
-            if (in_array($indice, $selectedIndices)) {
-                $choices_selected[] = array(
-                    'value' => $indice,
-                    'label' => $choices[$indice]
-                );
+        //Rebuilds choices
+        foreach ($this->choices as $key => $choice) {
+            if (isset($selecteds[$key])) {
+                $choices_selected[$key] = $choice;
             } else {
-                $choices_unselected[] = array(
-                    'value' => $indice,
-                    'label' => $choices[$indice]
-                );
+                $choices_unselected[$key] = $choice;
             }
         }
 
-        $view->set('choices_selected',  $choices_selected);
+        $view->set('choices_selected', $choices_selected);
         $view->set('choices_unselected', $choices_unselected);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getDefaultOptions(array $options)
+    public function getParent(array $options)
     {
-        $options = parent::getDefaultOptions($options);
-        $options['multiple'] = true;
-
-        return $options;
+        return 'field';
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    public function getDefaultOptions(array $options)
+    {
+        $defaultOptions = array(
+            'class'             => null,
+            'property'          => null,
+            'choices'           => array(),
+            'query_object'      => null,
+        );
+
+        $options = array_replace($defaultOptions, $options);
+
+        if (!isset($options['choice_list'])) {
+            $defaultOptions['choice_list'] = new ModelChoiceList(
+                $options['class'],
+                $options['property'],
+                $options['choices'],
+                $options['query_object']
+            );
+        }
+
+        return $defaultOptions;
+    }
+
     public function getName()
     {
         return 'propel_double_list';
