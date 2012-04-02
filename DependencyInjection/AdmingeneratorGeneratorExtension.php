bcbe3ea11bbd24b16d4b418c0a72ef6cc052d268
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2012-04-02 22:14:53 +0200

    Force use Symfony\Component\HttpFoundation\Session\Flash\FlashBag for flash bag See https://github.com/symfony/symfony/issues/3762#issuecomment-4887212

--- DependencyInjection/AdmingeneratorGeneratorExtension.php
+++ DependencyInjection/AdmingeneratorGeneratorExtension.php
@@ -35,6 +35,7 @@ class AdmingeneratorGeneratorExtension extends Extension
         $container->setParameter('admingenerator.overwrite_if_exists', $config['overwrite_if_exists']);
         $container->setParameter('admingenerator.base_admin_template', $config['base_admin_template']);
         $container->setParameter('admingeneretor.menu_builder.class', $config['knp_menu_class']);
+        $container->setParameter('session.flashbag.class', 'Symfony\Component\HttpFoundation\Session\Flash\FlashBag');
 
         $resources = $container->getParameter('twig.form.resources');
         $resources[] = 'AdmingeneratorGeneratorBundle:Form:fields.html.twig';
