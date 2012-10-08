49f2c85b2605d79753f8719fd41be8c2fd1c16ea
loostro <loostro@gmail.com>
2012-10-08 14:04:38 +0200

    Adding documentation + small fix.

--- Form/Type/EntityPickerType.php
+++ Form/Type/EntityPickerType.php
@@ -2,16 +2,6 @@
 
 namespace Admingenerator\GeneratorBundle\Form\Type;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityChoiceList;
-use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
-use Symfony\Bridge\Doctrine\Form\EventListener\MergeDoctrineCollectionListener;
-use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\Options;
-
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -28,11 +18,11 @@ class EntityPickerType extends EntityType
         parent::setDefaultOptions($resolver);
         
         $resolver->setDefaults(array(
-            'builder'     => array(),
-            'matcher'     => 'item',
-            'identifier'  => 'item',
+            'builder'     => array('pk' => 'id', 'name' => 'name'),
+            'matcher'     => 'item.name',
+            'identifier'  => 'item.id',
             'description' => array(),
-            'thumb'       => array(),
+            'thumb'       => array('src' => null, 'width' => '32px', 'height' => '32px'),
         ));
         
         $resolver->setAllowedTypes(array(
@@ -52,7 +42,6 @@ class EntityPickerType extends EntityType
         $view->vars['builder'] = $options['builder'];
         $view->vars['matcher'] = $options['matcher'];
         $view->vars['identifier'] = $options['identifier'];
-        $view->vars['placeholder'] = $options['placeholder'];
         $view->vars['description'] = $options['description'];
         $view->vars['thumb'] = $options['thumb'];
     }
