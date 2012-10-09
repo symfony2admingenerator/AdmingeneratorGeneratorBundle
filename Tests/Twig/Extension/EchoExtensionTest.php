24c30744885c319a3119a79f60968267287de657
loostro <loostro@gmail.com>
2012-10-09 12:07:16 +0200

    finally.. fixed
    i guess ill have to learn how to install ruby with gems and do the tests on my local pc, waiting for travisbot is too much pain in the ass

--- Tests/Twig/Extension/EchoExtensionTest.php
+++ Tests/Twig/Extension/EchoExtensionTest.php
@@ -50,7 +50,7 @@ class EchoExtensionTest extends TestCase
 
         $returns = array(
             'string' => array("cedric", 'i18n does not modify the string'),
-            'array' => array("array(  'obj' => 'val', 'translation_domain' => 'foo')", 'i18n succesfully adds translation_domain to the array'),
+            'array' => array("array(  'obj' => 'val',  'translation_domain' => 'foo',)", 'i18n succesfully adds translation_domain to the array'),
         );
 
        $this->runTwigTests($tpls, $returns);
