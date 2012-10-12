e5da82e9e859abb3da6c15d6150bfd4f79df6630
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2012-10-12 22:42:08 +0200

    Add yaml validator and skip if propel generated files not yet ready #242

---
+++ Validator/PropelModelClassValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Admingenerator\GeneratorBundle\Validator;
+
+use Admingenerator\GeneratorBundle\Generator\Generator;
+use Admingenerator\GeneratorBundle\Exception\GeneratedModelClassNotFoundException;
+
+class PropelModelClassValidator extends BaseValidator implements ValidatorInterface
+{
+	public function validate(Generator $generator)
+	{
+		$model = $this->getFromYaml($generator, 'params.model');
+		$parts = explode('\\', $model);
+		$modelName = $parts[sizeof($parts) -1];
+		unset($parts[sizeof($parts) -1]);
+
+		$model = implode('\\', $parts).'\\om\\Base'.$modelName;
+
+		if (!class_exists($model)) {
+			throw new GeneratedModelClassNotFoundException(sprintf('Unable to find class %s for %s', $model,  $generator->getGeneratorYml()));
+		}
+	}
+}
+
