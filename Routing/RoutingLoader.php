2e8d02e774ed8b757f450ee75e22028920f4689f
Virginie Bardales <nelim17@hotmail.com>
2012-10-22 18:26:52 +0300

    Update Routing/RoutingLoader.php
    Avoid conflict with composer intallation tree

--- Routing/RoutingLoader.php
+++ Routing/RoutingLoader.php
@@ -122,7 +122,7 @@ class RoutingLoader extends FileLoader
 
     protected function getNamespaceFromResource($resource)
     {
-        preg_match('#.+/(src|bundles)/(.+)/(.+Bundle)/Controller?/(.*?)/?$#', $resource, $matches);
+        preg_match('#.+/(.+)/(.+Bundle)/Controller?/(.*?)/?$#', $resource, $matches);
 
         return str_replace('/', '\\', $matches[2]);
     }
