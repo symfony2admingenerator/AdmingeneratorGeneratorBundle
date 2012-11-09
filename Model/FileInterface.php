423fbd6108713720f62390433a2aa5f2cb0c8768
loostro <loostro@gmail.com>
2012-11-09 14:05:36 +0100

    Updated file interface

--- Model/FileInterface.php
+++ Model/FileInterface.php
@@ -16,27 +16,56 @@ interface FileInterface
     public function __construct($file, $entity);
     
     /**
+     * Return entity unique identifier
+     * 
      * @return integer
      */
     public function getId();
 
     /**
+     * Return file name (e.g. image001.png)
+     * 
      * @return string
      */
     public function getName();
 
     /**
+     * Return file extension (e.g. png)
+     * 
      * @return string
+     */
+    public function getExtension();
+
+    /**
+     * Return file mimetype
+     * 
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * Return file size in bytes
+     * 
+     * @return integer
      */
     public function getSize();
 
     /**
-     * @return string
+     * Return true if file thumbnail should be generated
+     * 
+     * @return boolean
      */
     public function getPreview();
 
     /**
-     * @return string
+     * Set Symfony\Component\HttpFoundation\File\File
      */
-    public function getWebPath();
+    public function setFile();
+
+    /**
+     * Return instance of Symfony\Component\HttpFoundation\File\File
+     * 
+     * @return Symfony\Component\HttpFoundation\File\File
+     */
+    public function getFile();
 }
