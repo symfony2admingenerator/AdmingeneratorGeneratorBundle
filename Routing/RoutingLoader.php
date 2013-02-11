e02843f013c8db5b81874757c524836a8b134b65
Hector Hurtarte <hectorh30@gmail.com>
2013-02-11 12:36:47 -0600

    Added nested_move to RoutingLoader actions

--- Routing/RoutingLoader.php
+++ Routing/RoutingLoader.php
@@ -69,6 +69,12 @@ class RoutingLoader extends FileLoader
                     'requirements' => array(),
                     'controller'   => 'list',
                 ),
+        'nested_move' => array(
+                    'pattern' => '/nested_move/{dragged}/{action}/{dropped}',
+                    'defaults' => array(),
+                    'requirements' => array(),
+                    'controller' => 'list'
+                ),
     );
 
     public function load($resource, $type = null)
