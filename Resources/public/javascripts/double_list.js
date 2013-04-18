87e59f1e202623aef3d221fab9ca92d629bfe929
Enkaho <github@nko-systems.net>
2013-04-18 22:03:20 +0300

    Update double_list.js
    in some cases, the init function returned a null id and produced an error.

--- Resources/public/javascripts/double_list.js
+++ Resources/public/javascripts/double_list.js
@@ -2,9 +2,12 @@ var sfDoubleList =
 {
   init: function(id, className)
   {
+    if(id == null)
+    return;
+    
     form = sfDoubleList.get_current_form(id);
 
-    callback = function() { sfDoubleList.submit(form, className) };
+      callback = function() { sfDoubleList.submit(form, className) };
 
     if (form.addEventListener)
     {
