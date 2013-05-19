adf1ce0332931b3491baffb466aaa92ed226f026
loostro <loostro@gmail.com>
2013-05-19 14:39:17 +0100

    Add jquery.browser code forked from this gist (http://gist.github.com/EaterOfCorpses/5081294)

---
+++ Resources/public/js/jquery/jquery.browser.js
@@ -0,0 +1,37 @@
+(function( jQuery, window, undefined ) {
+    "use strict";
+
+    var matched, browser;
+    
+    jQuery.uaMatch = function( ua ) {
+        ua = ua.toLowerCase();
+        var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        [];
+        return {
+            browser: match[ 1 ] || "",
+            version: match[ 2 ] || "0"
+        };
+    };
+    
+    matched = jQuery.uaMatch( window.navigator.userAgent );
+    
+    browser = {};
+    if ( matched.browser ) {
+        browser[ matched.browser ] = true;
+        browser.version = matched.version;
+    }
+
+    // Chrome and Safari > 12.14 is Webkit, but Webkit is also Safari.
+    if ( browser.chrome || (browser.opera && parseFloat(browser.version) > 12.14 ) ) {
+        browser.webkit = true;
+    } else if ( browser.webkit ) {
+        browser.safari = true;
+    }
+    
+    jQuery.browser = browser;
+    
+})( jQuery, window );
