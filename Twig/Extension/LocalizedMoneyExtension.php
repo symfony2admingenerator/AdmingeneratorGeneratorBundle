5692a17115347641f10bf517550042c95dff538f
loostro <loostro@gmail.com>
2013-06-06 11:23:06 +0100

    Fix localized money extension Introduce currency_sign twig function

--- Twig/Extension/LocalizedMoneyExtension.php
+++ Twig/Extension/LocalizedMoneyExtension.php
@@ -6,31 +6,51 @@ use Symfony\Component\Form\Extension\Core\DataTransformer\MoneyToLocalizedString
 
 class LocalizedMoneyExtension extends \Twig_Extension
 {
-    public function getFilters()
+    public function getFunctions()
     {
         return array(
-            'currency' => new \Twig_Filter_Method($this, 'currency'),
+            'localized_money' => new \Twig_Function_Method($this, 'getLocalizedMoney'),
+            'currency_sign'   => new \Twig_Function_Method($this, 'getCurrencySign'),
         );
     }
 
     /**
-     * @param float  $value     Money amount
-     * @param string $options   Money options
+     * @param float  $value       Money amount.
+     * 
+     * @param string $currency    This can be any 3 letter ISO 4217 code. You 
+     * can also set this to false to hide the currency symbol.
+     * 
+     * @param string $precision   For some reason, if you need some precision 
+     * other than 2 decimal places, you can modify this value. You probably 
+     * won't need to do this unless, for example, you want to round to the 
+     * nearest dollar (set the precision to 0).
+     * 
+     * @param string $grouping    This value is used internally as the 
+     * NumberFormatter::GROUPING_USED value when using PHP's NumberFormatter 
+     * class. Its documentation is non-existent, but it appears that if you set 
+     * this to true, numbers will be grouped with a comma or period (depending 
+     * on your locale): 12345.123 would display as 12,345.123.
+     * 
+     * @param string $divisor     If, for some reason, you need to divide your 
+     * starting value by a number before rendering it to the user, you can use 
+     * the divisor option.
      *
      * @return string Localized money
      */
-    public function currency($value, $options)
+    public function getLocalizedMoney($value, $currency = 'EUR', $precision = 2, $grouping = true, $divisor = 1)
     {
-        $format = new \NumberFormatter('en', \NumberFormatter::CURRENCY);
-        $pattern = $format->formatCurrency('123', $options['currency']);
+        $locale = \Locale::getDefault();
         
-        $dt = new MoneyToLocalizedStringTransformer($options['precision'], $options['grouping'], null, $options['divisor']);
+        $format = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        $pattern = $format->formatCurrency('123', $currency);
+        
+        $dt = new MoneyToLocalizedStringTransformer($precision, $grouping, null, $divisor);
         $transformed_value = $dt->transform($value);
         
         preg_match('/^([^\s\xc2\xa0]*)[\s\xc2\xa0]*123(?:[,.]0+)?[\s\xc2\xa0]*([^\s\xc2\xa0]*)$/u', $pattern, $matches);
         
         if (!empty($matches[1])) {
-            $localized_money = $transformed_value.' '.$matches[1];
+            $localized_money = $matches[1].' '.$transformed_value;
         } elseif (!empty($matches[2])) {
             $localized_money = $transformed_value.' '.$matches[2];
         } else {
@@ -38,6 +58,32 @@ class LocalizedMoneyExtension extends \Twig_Extension
         }
         
         return $localized_money;
+    }
+
+    /**
+     * @param string $currency    This can be any 3 letter ISO 4217 code. You 
+     * can also set this to false to return the general currency symbol.
+     *
+     * @return string Currency sign
+     */
+    public function getCurrencySign($currency = false)
+    {
+        $locale = \Locale::getDefault();
+        
+        $format = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        $pattern = $format->formatCurrency('123', $currency);
+        
+        preg_match('/^([^\s\xc2\xa0]*)[\s\xc2\xa0]*123(?:[,.]0+)?[\s\xc2\xa0]*([^\s\xc2\xa0]*)$/u', $pattern, $matches);
+        
+        if (!empty($matches[1])) {
+            $currency_sign = $matches[1];
+        } elseif (!empty($matches[2])) {
+            $currency_sign = $matches[2];
+        } else {
+            $currency_sign = '¤';
+        }
+        
+        return $currency_sign;
     }
 
     /**
