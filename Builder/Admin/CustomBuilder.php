533bdcf3fb3e3d3b84d42c812424ab3bbf8fc815
loostro <loostro@gmail.com>
2013-06-15 23:03:59 +0100

    Change actions to object actions

--- Builder/Admin/CustomBuilder.php
+++ Builder/Admin/CustomBuilder.php
@@ -16,4 +16,44 @@ class CustomBuilder extends BaseBuilder
     {
         return 'custom';
     }
+
+    /**
+     * Return a list of action from list.object_actions
+     * @return array
+     */
+    public function getObjectActions()
+    {
+        if (0 === count($this->object_actions)) {
+            $this->findObjectActions();
+        }
+
+        return $this->object_actions;
+    }
+    
+    protected function setUserObjectActionConfiguration(Action $action)
+    {
+        $options = $this->getVariable(sprintf('object_actions[%s]', $action->getName()),array(), true);
+
+        if (null !== $options) {
+            foreach ($options as $option => $value) {
+                $action->setProperty($option, $value);
+            }
+        }
+    }
+
+    protected function addObjectAction(Action $action)
+    {
+        $this->object_actions[$action->getName()] = $action;
+    }
+
+    protected function findObjectActions()
+    {
+        foreach ($this->getVariable('object_actions') as $actionName => $actionParams) {
+            $action = $this->findObjectAction($actionName);
+            if(!$action) $action = new Action($actionName);
+
+            $this->setUserObjectActionConfiguration($action);
+            $this->addObjectAction($action);
+        }
+    }
 }
