56e637da4c82723ab1bd6a8c0c0192a9dfca0f42
Montabou <montabou@gmail.com>
2013-08-24 17:59:58 +0200

    add class comment to DefaultController/Type for fixing coding standard

--- Resources/skeleton/bundle/DefaultType.php
+++ Resources/skeleton/bundle/DefaultType.php
@@ -4,6 +4,9 @@ namespace {{ namespace }}\Form\Type{{ prefix ? "\\" ~ prefix : "" }};
 
 use Admingenerated\{{ bundle }}\Form\Base{{ prefix }}Type\{{ form }}Type as Base{{ form }}Type;
 
+/**
+ * {{ form }}Type
+ */
 class {{ form }}Type extends Base{{ form }}Type
 {
 }
