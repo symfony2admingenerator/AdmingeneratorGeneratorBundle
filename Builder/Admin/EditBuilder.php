63f830f4dc29b9096f2f6b500de36b9208c68c87
Stéphane Escandell <stephane.escandell@gmail.com>
2013-12-24 15:52:24 +0100

    FIX issue #646

--- Builder/Admin/EditBuilder.php
+++ Builder/Admin/EditBuilder.php
@@ -20,19 +20,4 @@ class EditBuilder extends BaseBuilder
     {
         return 'edit';
     }
-
-    /**
-     * Find form actions
-     */
-    protected function findActions()
-    {
-        foreach ($this->getVariable('actions', array()) as $actionName => $actionParams) {
-            $action = $this->findGenericAction($actionName);
-            if(!$action) $action = $this->findObjectAction($actionName);
-            if(!$action) $action = new Action($actionName);
-
-            $this->setUserActionConfiguration($action);
-            $this->addAction($action);
-        }
-    }
 }
