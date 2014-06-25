9f8ec8eb74b25d4f281f3e966659b4cb2b219e65
Restless-ET <restless86@gmail.com>
2014-06-25 17:59:11 +0100

    Add support to an array of values on filters for DoctrineODM.

--- QueryFilter/DoctrineODMQueryFilter.php
+++ QueryFilter/DoctrineODMQueryFilter.php
@@ -7,7 +7,11 @@ class DoctrineODMQueryFilter extends BaseQueryFilter
 
     public function addDefaultFilter($field, $value)
     {
-        $this->query->field($field)->equals($value);
+        if (is_array($value)) {
+            $this->query->field($field)->in($value);
+        } else {
+            $this->query->field($field)->equals($value);
+        }
     }
 
     public function addStringFilter($field, $value)
